eee1c3ce5072e6c0f39eefa4f3cabb88d4f71c91
Kacper Cygan <cygan.kacper@outlook.com>
2022-08-25 23:12:30 +0200

    Minor steps with Thymeleaf

--- src/main/java/com/example/Controllers/view/WebController.java
+++ src/main/java/com/example/Controllers/view/WebController.java
@@ -1,15 +1,17 @@
 package com.example.Controllers.view;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/home")
 public class WebController {
-
-	@GetMapping("/showForm")
-	public String getView() {
-		return "index";
+	
+	@GetMapping("")
+	public String getView(Model theModel) {
+		theModel.addAttribute("localTime", new java.util.Date());
+		return "helloworld";
 	}	
 }
