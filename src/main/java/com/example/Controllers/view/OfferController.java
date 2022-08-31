803b7308e1defea2193e0be7b20f4a5f131225e4
Kacper Cygan <cygan.kacper@outlook.com>
2022-08-31 21:53:56 +0200

    Demo tests

---
+++ src/main/java/com/example/Controllers/view/OfferController.java
@@ -0,0 +1,43 @@
+package com.example.Controllers.view;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.Entities.Customer;
+import com.example.Entities.Product;
+import com.example.Service.CustomerService;
+import com.example.Service.ProductService;
+
+@Controller
+@RequestMapping("/home")
+public class OfferController {
+
+	@Autowired
+	CustomerService customerService;
+	
+	@Autowired
+	ProductService productService;
+	
+	@GetMapping("")
+	public String getView(Model theModel) {
+		List<Product> retrievedProducts = productService.listAll();
+		theModel.addAttribute("products", retrievedProducts);
+		return "helloworld";
+	}
+	
+	@GetMapping("/showOffer/{offerId}")
+	public String getView(@PathVariable int offerId, Model theModel) {
+		Product retrievedProduct = productService.findById(offerId);
+		theModel.addAttribute("product", retrievedProduct);
+		return "showoffers";
+	}
+}
